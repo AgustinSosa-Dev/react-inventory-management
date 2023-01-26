4cc84867c5e05c7ffc0a1ca1a46211c03aa2b42d
Agustín Sosa-Dev <agustinsosa.dev@gmail.com>
2023-01-26 12:04:50 -0300

    Deploy on Render

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://react-inventory-management.vercel.app/",
+    ],
     credentials: true,
   })
 );
